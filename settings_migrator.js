d92a812c40d1b05351f9c0ba4a5f76935ca1de27
Enndy1005 <vetalbabak1987@gmail.com>
2024-11-22 00:32:49 +0200

    Update settings_migrator.js

--- settings_migrator.js
+++ settings_migrator.js
@@ -1,37 +1,36 @@
-/* eslint-disable no-param-reassign */
 "use strict";
 
 const DefaultSettings = {
     hotkey: "Ctrl+Shift+P",
-    enabled: true
+    enabled: true,
+	coloredOutput: true
 };
 
-
 module.exports = function MigrateSettings(from_ver, to_ver, settings) {
-	if (from_ver === undefined) return { ...DefaultSettings, ...settings };
-	else if (from_ver === null) return DefaultSettings;
-	else {
-		from_ver = Number(from_ver);
-		to_ver = Number(to_ver);
+    if (from_ver === undefined) return { ...DefaultSettings, ...settings };
+    else if (from_ver === null) return DefaultSettings;
+    else {
+        from_ver = Number(from_ver);
+        to_ver = Number(to_ver);
 
-		if (from_ver + 1 < to_ver) {
-			settings = MigrateSettings(from_ver, from_ver + 1, settings);
-			return MigrateSettings(from_ver + 1, to_ver, settings);
-		}
+        if (from_ver + 1 < to_ver) {
+            settings = MigrateSettings(from_ver, from_ver + 1, settings);
+            return MigrateSettings(from_ver + 1, to_ver, settings);
+        }
 
-		const oldsettings = settings;
+        const oldsettings = settings;
 
-		switch (to_ver) {
-			default:
-				settings = Object.assign(DefaultSettings, {});
+        switch (to_ver) {
+            default:
+                settings = Object.assign(DefaultSettings, {});
 
-				for (const option in oldsettings) {
-					if (settings[option] !== undefined) {
-						settings[option] = oldsettings[option];
-					}
-				}
-		}
+                for (const option in oldsettings) {
+                    if (settings[option] !== undefined) {
+                        settings[option] = oldsettings[option];
+                    }
+                }
+        }
 
-		return settings;
-	}
+        return settings;
+    }
 };
